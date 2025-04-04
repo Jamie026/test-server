1483efd1c245334bd325ab29e0cd6e59d84d9460
Jamie Nuñez <jamienunez02@gmail.com>
2025-04-03 23:46:34 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -8,22 +8,40 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT);
 app.use(cors());
 
+let clients = [];
+
 app.get('/events', (req, res) => {
-    // CORS headers:
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
 
     res.write('data: Hola cliente 👋\n\n');
 
-    const interval = setInterval(() => {
-        res.write(`data: Actualización: ${new Date().toISOString()}\n\n`);
-    }, 3000);
+    clients.push(res);
 
-    req.on('close', () => clearInterval(interval));
+    req.on('close', () => {
+        clients = clients.filter(client => client !== res);
+    });
 });
 
+app.post("/notify", async (req, res) => {
+    try {
+        console.log("Notificación recibida.");
+        const newData = await getData();
+        console.log('🔄 Datos actualizados, enviando mensaje a los clientes...');
+
+        // Envía los datos a todos los clientes conectados
+        clients.forEach(client => {
+            client.write(`data: ${JSON.stringify({ type: 'update', data: newData })}\n\n`);
+        });
+
+        res.send({ message: 'Ok' });
+    } catch (error) {
+        console.error("Error al procesar la notificación:", error);
+        res.status(500).send({ error: 'Error al procesar la notificación' });
+    }
+});
 
 module.exports = { app, server };
