3c963fca8e0318e6789a5a103ad7e1bbbe26cc22
Jamie Nuñez <jamienunez02@gmail.com>
2025-04-04 00:41:02 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT);
-app.use(cors());
+app.use(cors({
+    origin: '*', // o el dominio que necesites
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+}));
 
 let clients = {};  // Objeto para almacenar las conexiones activas por un identificador único, por ejemplo, userId
 
 app.get('/events', (req, res) => {
-    const userId = req.query.userId;  // Obtener el identificador único del cliente, por ejemplo, desde los parámetros de la URL
+    const userId = req.query.userId;
+    console.log("userId recibido:", userId);  // Agrega un log aquí para verificar
     if (!userId || clients[userId]) {
-        // Si el cliente ya está registrado, cierra la conexión
         res.status(400).send('Ya tienes una conexión activa.');
         return;
     }
-
-    // Registrar la nueva conexión
     clients[userId] = res;
 
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -32,7 +35,6 @@ app.get('/events', (req, res) => {
         delete clients[userId];  // Eliminar al cliente cuando se desconecte
     });
 });
-
 
 app.post("/notify", async (req, res) => {
     try {
